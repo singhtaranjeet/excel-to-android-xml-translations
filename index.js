b59cd9c809f3de102d22d39c6ea66e21caeed91b
Taranjeet Singh <taranjeet@flick2know.com>
2023-05-24 14:14:22 +0530

    initial commit
    Signed-off-by: Taranjeet Singh <taranjeet@flick2know.com>

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const XLSX = require("xlsx");
+const xmlbuilder = require("xmlbuilder");
+const path = require("path");
+
+const workbook = XLSX.readFile("gt-app-muti-lang.xlsx");
+const sheetName = workbook.SheetNames[0];
+const worksheet = workbook.Sheets[sheetName];
+const xmlFileName = "ta-strings.xml";
+
+const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+const xmlRoot = xmlbuilder.create("resources");
+
+jsonData.forEach((row) => {
+  const key = row["Key"];
+  const translation = row["ta"] || row["English"];
+  xmlRoot.ele("string", { name: key }, translation);
+});
+
+// Convert the XML to a string
+const xmlString = xmlRoot.end({ pretty: true });
+const folderName = "translations";
+const translationsFolderPath = path.join(__dirname, folderName);
+
+if (!fs.existsSync(translationsFolderPath)) {
+  fs.mkdirSync(translationsFolderPath);
+}
+
+const xmlFilePath = path.join(translationsFolderPath, xmlFileName);
+
+// Write the XML to a file
+fs.writeFileSync(xmlFilePath, xmlString, "utf8");
+
+console.log(`Translations are ready in ${xmlFileName}`);
